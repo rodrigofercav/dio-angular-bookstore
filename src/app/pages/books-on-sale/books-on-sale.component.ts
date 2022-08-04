af5bdd3da1b5b206a43d47d6102a02489d0e7dc0
rodrigofercav <rodrigofercav@gmail.com>
2022-08-04 01:33:59 -0300

    add book pages

---
+++ src/app/pages/books-on-sale/books-on-sale.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-books-on-sale',
+    templateUrl: './books-on-sale.component.html'
+})
+export class BooksOnSaleComponent {
+    constructor() { }
+}
---
+++ src/app/pages/books/books.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-books',
+    templateUrl: './books.component.html'
+})
+export class BooksComponent {
+    constructor() { }
+}
